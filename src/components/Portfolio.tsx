7eedb8dafba5589777a523b400576f63869d54c1
Atheek <atheek163@gmail.com>
2025-07-20 17:26:53 +0530

    Update Portfolio.tsx

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -7,7 +7,7 @@ import Contact from './Contact';
 import ThemeToggle from './ThemeToggle';
 import Experience from './Experience';
 import Navbar from './Navbar';
-import { Analytics } from "@vercel/analytics/next"
+
 
 const Portfolio = () => {
   const ref = useRef(null);
@@ -120,7 +120,7 @@ const Portfolio = () => {
         </motion.footer>
       </div>
       {/* Analytics */}
-      <Analytics />
+      
     </div>
   );
 };
